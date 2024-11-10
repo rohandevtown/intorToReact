3fb2c061e46b4ed3cd6168d53a047dd5dcd472f0
rohandevtown <rohankinnal@devtown.in>
2024-11-10 16:49:07 +0530

    d1

---
+++ react_proj/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+// import ClassComponent from './components/ClassComponent';
+// import FunctionalComponent from './components/FunctionalComponent';
+
+// React Slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello Rohan</h1>
+      <ClassComponent />
+      <FunctionalComponent /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react_proj/src/components/FunctionalComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function FunctionalComponent(){
+    return(
+        <div>
+            <h1>Functional Component</h1>
+        </div>
+    )
+}
+
+export default FunctionalComponent
+
+
+// import React from 'react'
+
+// const FunctionalComponent = () => {
+//   return (
+//     <div>FunctionalComponent</div>
+//   )
+// }
+
+// export default FunctionalComponent
